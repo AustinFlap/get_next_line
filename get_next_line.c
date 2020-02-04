7e5caca391c32fea411cb47710c0557134348340
AustinFlap <56319429+AustinFlap@users.noreply.github.com>
2020-02-04 07:11:03 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,30 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 18:24:13 by avieira           #+#    #+#             */
-/*   Updated: 2020/02/04 05:48:15 by avieira          ###   ########.fr       */
+/*   Updated: 2020/02/04 07:10:19 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*append_line(char *buff, char *line, int *new_line)
+char	*append_line(char *buff, char *stock, int *new_line)
 {
 	int l_to_end;
 	int l_buff;
-	char *temp_line;
+	char *temp_stock;
 	char *temp_substr;
 	
 	l_to_end = ft_strl_spec(buff, '\n');
 	l_buff = ft_strl_spec(buff, 0);
 	temp_substr = ft_substr(buff, 0, l_to_end);
-	temp_line = ft_strjoin(line, temp_substr);
-	free(line);
+	temp_stock = ft_strjoin(stock, temp_substr);
+	free(stock);
 	free(temp_substr);
-	line = temp_line;
+	stock = temp_stock;
 	*new_line = (buff[l_to_end] == '\n') ? 1 : 0;
 	ft_memcpy(buff, buff + l_to_end + *new_line, l_buff - l_to_end - *new_line + 1);
-	return (line);
+	return (stock);
 }
 
 
@@ -38,36 +38,41 @@ int		get_next_line(int fd, char **line)
 	static char		buff[MAX_FD][BUFFER_SIZE + 1];
 	int				ret;
 	int				new_line;
+	char *stock;
 
 	if (!line || !(MAX_FD > fd) || !BUFFER_SIZE)
 		return (-1);
-	*line = ft_strdup("");
+	stock = ft_strdup("");
 	new_line = 0;
 	while (!new_line)
 	{
 		if (!*buff[fd])
 		{
-			if ((ret = read(fd, buff[fd], BUFFER_SIZE)) == -1)
+			if (!((ret = read(fd, buff[fd], BUFFER_SIZE)) > 0))
 				return (ret);
 			buff[fd][ret] = 0;
 		}
-		*line = append_line(buff[fd], *line, &new_line);
-		if (ret == 0)
-			new_line = 1;
+		stock = append_line(buff[fd], stock, &new_line);
+		/*if (ret < BUFFER_SIZE)
+			new_line = 1;*/  //INFLUE PRINCIPALEMENT POUR BUFFER SIZE == 1
 	}
-	if (!ret)
-		return (0);
+	*line = stock;
 	return (1);
 }
-/*
+
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 int main(void)
 {
-	int fd = open("files/42_char", O_RDONLY);
-	char *line = 0;
-	while (get_next_line(fd, &line))
+	int fd = open("files/mix_marge2", O_RDONLY);
+	char *line;
+	while (get_next_line(fd, &line) > 0)
+	{
 		printf("%s\n", line);
-	printf("%s\n", line);
-}*/
+		free(line);
+	}
+	if (line)
+		printf("%s\n", line);
+	close(fd);
+}
