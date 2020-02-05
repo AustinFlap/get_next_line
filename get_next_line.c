f7311e1a504a0ab981cda511ec7f4506c597362c
AustinFlap <56319429+AustinFlap@users.noreply.github.com>
2020-02-05 22:35:57 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 18:24:13 by avieira           #+#    #+#             */
-/*   Updated: 2020/02/04 07:10:19 by avieira          ###   ########.fr       */
+/*   Updated: 2020/02/05 22:35:02 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ char	*append_line(char *buff, char *stock, int *new_line)
 	free(stock);
 	free(temp_substr);
 	stock = temp_stock;
-	*new_line = (buff[l_to_end] == '\n') ? 1 : 0;
+	*new_line = (buff[l_to_end] == '\n') ? 1 : *new_line;
+	//if (!ret)
+	//	new_line = 0;
 	ft_memcpy(buff, buff + l_to_end + *new_line, l_buff - l_to_end - *new_line + 1);
 	return (stock);
 }
@@ -48,15 +50,21 @@ int		get_next_line(int fd, char **line)
 	{
 		if (!*buff[fd])
 		{
-			if (!((ret = read(fd, buff[fd], BUFFER_SIZE)) > 0))
+			if (!((ret = read(fd, buff[fd], BUFFER_SIZE)) > -1))
 				return (ret);
 			buff[fd][ret] = 0;
+			new_line = (ret < BUFFER_SIZE) ? 1 : 0;
 		}
 		stock = append_line(buff[fd], stock, &new_line);
-		/*if (ret < BUFFER_SIZE)
-			new_line = 1;*/  //INFLUE PRINCIPALEMENT POUR BUFFER SIZE == 1
+		//printf("|new_line%d| |ret%d|------\n%s\n", new_line, ret, buff[fd]);
 	}
 	*line = stock;
+	if (JESAISPAS)
+	{
+		printf("|ret %d|   |new_line %d|\n", ret, new_line);
+		return (0);
+	}
+	//printf("|%d|\n",ret);
 	return (1);
 }
 
@@ -72,7 +80,5 @@ int main(void)
 		printf("%s\n", line);
 		free(line);
 	}
-	if (line)
-		printf("%s\n", line);
 	close(fd);
 }
